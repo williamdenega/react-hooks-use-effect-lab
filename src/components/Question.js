6cf22897e80ef9a3a08560f1705cc22b0f954ea6
WilliamDenega <williamdenega@gmail.com>
2022-05-02 16:29:01 -0400

    commit tree 36efe4aadf8420e92bcfe0da5ac863aaaa63c793

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  // Made usuing a useCallback (useCallBack)
+  // const changeTime = useCallback(() => {
+  //   if(timeRemaining !== 0){
+  //     setTimeRemaining((timeRemaining)=>timeRemaining-1)
+  //   }else{
+  //     setTimeRemaining(3)
+  //     onAnswered(false)
+  //   }
+  // }, [timeRemaining, onAnswered]);  // ✅ Callback deps are OK
+
+
+  useEffect(()=>{
+    // way using a useCallBack
+    //const timer = setTimeout((changeTime),1000)
+
+    const timer = setTimeout((()=>{
+      if(timeRemaining !== 0){
+        setTimeRemaining((timeRemaining)=>timeRemaining-1)
+      }else{
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+  
+
+    }), 1000)
+    
+    return ()=> clearTimeout(timer)
+  },[timeRemaining, onAnswered])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
